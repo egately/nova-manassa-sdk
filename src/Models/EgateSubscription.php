93c74d437f625e5dd66e725a149aa35d9d65b6cd
Salah <salah@elshaden.com>
2022-08-22 13:31:17 +0100

    Sushi

--- src/Models/EgateSubscription.php
+++ src/Models/EgateSubscription.php
@@ -6,6 +6,7 @@ namespace Egately\NovaManassaSdk\Models;
 use Egately\NovaManassaSdk\NovaManassaSdk;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Sushi\Sushi;
 
 class EgateSubscription extends Model
@@ -17,33 +18,39 @@ class EgateSubscription extends Model
         return $this->ImportRows();
     }
 
-    protected function sushiShouldCache()
-    {
-        return true;
-    }
-
-    protected function sushiCacheReferencePath()
-    {
-
-        return storage_path('app/manassa/tables').'/table.csv';
-    }
+//    protected function sushiShouldCache()
+//    {
+//        return false;
+//    }
+//
+//    protected function sushiCacheReferencePath()
+//    {
+//        $stg = storage_path('app/manassa');
+//
+//        if (file_exists($stg)) {
+//            'yes';
+//        } else {
+//            'no';
+//        }
+//        return storage_path('app/') . 'table.csv';
+//    }
 
 
     public function ImportRows()
     {
-        if(auth()->user()->manassaAccount->manassa_id == null) {
-            $data = ['account_id' =>auth()->user()->manassaAccount->manassa_id];
+        $account = auth()->user()?->manassaAccount?->manassa_id ?? Null;
+        if ($account != null) {
+            $data = ['account_id' => auth()->user()->manassaAccount->manassa_id];
             $Rows = app(NovaManassaSdk::class)->syncSubscritions($data);
             Log::info('ImportRows', [$Rows]);
-            if(isset($Rows['data'])) {
+            if (isset($Rows['data'])) {
                 return $Rows['data'];
             }
             return [];
-        }else {
-          return [];
+        } else {
+            return [];
         }
     }
-
 
 
 }
