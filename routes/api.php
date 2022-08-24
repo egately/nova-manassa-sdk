650ae680f4ce68ba2a6eea14ff25f6fb21876512
Salah <salah@elshaden.com>
2022-08-24 20:05:58 +0100

    Updated Subscription Sync URL

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('api/v1/manassa/status-changed/{account}',[config('nova-manassa-sdk.manassa_status_change_controller_class'),
+Route::post('api/v1/manassa/status-changed',[config('nova-manassa-sdk.manassa_status_change_controller_class'),
     config('nova-manassa-sdk.manassa_status_change_controller_method')]);
